35ab50f4a9907dc0f34b2e7435ca9b0532e3aa5b
AJkz17 <magicmoon1344@gmail.com>
2026-09-20 12:24:33 +0800

    stack layout; allow user to navigate to ProductCard.tsx once click in the item container

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
-import HomeScreen from './index';
 
 SplashScreen.preventAutoHideAsync();
 
-export default function TabLayout() {
+export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
-
-      <HomeScreen />
-
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="product/[id]" options={{ title: 'Product Details' }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
